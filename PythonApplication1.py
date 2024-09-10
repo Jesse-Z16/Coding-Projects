740663f18afd37b0c072d784161dc3bcd1c62e55
Jesse-Z16 <nziaraba@gmail.com>
2024-09-10 11:10:31 +0100

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,20 @@
+
+#x = 'Hello world'
+#y = 'This is my first program'
+#print(x,y)
+
+#print(type(x))
+
+def carpet_cost(width, length, price):
+    carpet = width * length * price
+    grippers = width + length
+    fitting = 50
+    return carpet + grippers + fitting
+    
+# -------------------------
+# Main program
+# -------------------------
+width = int(input("Enter the width of the room to nearest meter: "))
+length = int(input("Enter the length of the room to nearest meter: "))
+price = float(input("Enter the price of the carpet per m2: "))
+print("The total cost is: £", carpet_cost(width, length, price))
